6d2995ab1e9c0b4b3e57c609dbe19bd3be6f6e42
Cipriana <ciprianaluis264@gmail.com>
2022-05-17 23:20:04 -0300

    Modificando UsuárioLogin e SwaggerConfig

--- src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
+++ src/main/java/com/generation/blogpessoal/configuration/SwaggerConfig.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
+@Configuration
 public class SwaggerConfig {
 
 	@Bean
